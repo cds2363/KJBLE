1a842a0b092a0df596faad096ade63093237f5c5
DongSu Cha <cds2363+github@gmail.com>
2016-03-29 17:40:55 +0900

    Initial Commit

---
+++ KJBLE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KJBLE
+//
+//  Created by chadongsu on 2016/03/29.
+//  Copyright (c) 2016年 chadongsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
---
+++ KJBLETests/KJBLETests.m
@@ -0,0 +1,40 @@
+//
+//  KJBLETests.m
+//  KJBLETests
+//
+//  Created by chadongsu on 2016/03/29.
+//  Copyright (c) 2016年 chadongsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface KJBLETests : XCTestCase
+
+@end
+
+@implementation KJBLETests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
